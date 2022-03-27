935eec2a8497804fd124082adb94912b8814e7a2
ApiwatHantrakool <apiwat@simyasolutions.com>
2022-03-27 19:07:50 +0700

    Server response

--- basic/app.js
+++ basic/app.js
@@ -3,6 +3,10 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   console.log(req);
   // process.exit(); // kill the process after the request.
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<h1>Hello World</h1>');
+  res.write('Hello World: ' + new Date());
+  res.end();
 });
 
 server.listen(3000); // Listen on port 3000.
