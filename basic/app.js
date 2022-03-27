d580e89b756b4b30e40fddad3ba957387ef7ed42
ApiwatHantrakool <apiwat@simyasolutions.com>
2022-03-27 15:46:04 +0700

    Exit server

--- basic/app.js
+++ basic/app.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   console.log(req);
+  // process.exit(); // kill the process after the request.
 });
 
 server.listen(3000); // Listen on port 3000.
