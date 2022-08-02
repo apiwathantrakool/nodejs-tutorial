a76948d9f84c0333d87693300b57b5ef1512b66f
nekkytechcombine <nekky@techcombine.co>
2022-08-02 17:29:23 +0700

    Basic: Routing request

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Run
+
+node app.js
+
 # Basic
 
 HTTP header doc = https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers
--- basic/app.js
+++ basic/app.js
@@ -1,11 +1,31 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   console.log(req);
   // process.exit(); // kill the process after the request.
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title><head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
   res.setHeader('Content-Type', 'text/html');
-  res.write('<h1>Hello World</h1>');
-  res.write('Hello World: ' + new Date());
+  res.write('<html>');
+  res.write('<head><title>My First Page</title><head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
   res.end();
 });
 
